b7f6257fbb0fa5fd55843424133bfddcd4a1c6b4
Ramon Andrade Sousa Carvalho <ramon.andrade@live.com>
2024-07-14 11:15:23 -0300

    Add error checking to the uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-sudo rm -rf "${HOME}"/.local/share/fonts/*
+echo -e "\n\033[32mNerd-Font Uninstaller\033[0m\n"
+
+echo -e "Removing all nerd fonts..."
+
+if rm -rf "${HOME}"/.local/share/fonts/* ; then
+
+  echo -e "Nerd fonts successfully removed!\n"
+
+else
+
+  echo -e "Nerd fonts removal failed.\n"
+
+fi
